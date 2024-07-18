eafdc3736e1202934511a300890115777d91251d
Kihwan <eljsh95@naver.com>
2024-07-18 18:50:47 +0900

    ㅠㅠ

--- js/main.js
+++ js/main.js
@@ -1,13 +1,60 @@
+const theme = {
+    primary: '#ecceae',
+    secondary: '#26355d',
+};
+
+function changeTheme(themeMode = 'dark') {
+    const tween = gsap
+        .timeline()
+        .to('body, .header', {
+            backgroundColor: themeMode === 'dark' ? theme.secondary : '#eee',
+            color: themeMode === 'dark' ? theme.primary : theme.secondary,
+            borderColor: themeMode === 'dark' ? '#fff' : theme.secondary,
+        })
+        .to(
+            '.intro_text p',
+            {
+                color: themeMode === 'dark' ? theme.primary : theme.secondary,
+            },
+            0
+        );
+
+    return tween;
+}
+
+function headerFixed() {
+    ScrollTrigger.create({
+        trigger: '.header',
+        start: 'top top',
+        end: 'max',
+        pin: true,
+        pinSpacing: false,
+        //markers: true,
+    });
+}
+
 function mainBgAnimation() {
     const mainBook = gsap.utils.toArray('.main .book');
 
     mainBook.forEach((elem, index) => {
-        gsap.to('.book div', {
-            yPercent: gsap.utils.wrap([50, -80]),
-            duration: 50,
-            repeat: -1,
-            ease: 'none',
-        });
+        console.log(elem);
+        gsap.timeline()
+            .set('.book div img', {
+                filter: 'blur(10px)',
+            })
+            .to('.book div img', {
+                filter: 'blur(0)',
+                stagger: {
+                    amount: 1.5,
+                    from: 'random',
+                },
+            })
+            .to('.book div', {
+                yPercent: gsap.utils.wrap([50, -80]),
+                duration: 100,
+                repeat: -1,
+                ease: 'none',
+            });
     });
 }
 
@@ -19,12 +66,18 @@ function scrollAnimation() {
         ease: 'none',
     });
 
-    const scrollAni2 = gsap.to('.scroll_star', {
-        rotate: -360,
-        repeat: -1,
-        duration: 20,
-        ease: 'none',
-    });
+    const scrollAni2 = gsap
+        .timeline()
+        .set('.scroll_star', {
+            xPercent: -50,
+            yPercent: -50,
+        })
+        .to('.scroll_star', {
+            rotate: -360,
+            repeat: -1,
+            duration: 20,
+            ease: 'none',
+        });
 
     ScrollTrigger.create({
         trigger: '.mainSection',
@@ -42,7 +95,7 @@ function scrollAnimation() {
         start: 'top center',
         end: 'bottom top',
         animation: gsap.to('.scroll_star img', {
-            rotate: -520,
+            rotate: -720,
             ease: 'none',
         }),
         scrub: true,
@@ -60,22 +113,70 @@ function contactMouseLeave() {
 text.addEventListener('mouseenter', contactMouseEnter);
 text.addEventListener('mouseleave', contactMouseLeave);
 
-function headerFixed() {
+function bodyBgAnimation() {
+    const bodyAni = gsap.to('body', {});
+
     ScrollTrigger.create({
-        trigger: '.header',
+        trigger: '.intro',
         start: 'top top',
-        end: 'max',
-        pin: true,
-        pinSpacing: false,
-<<<<<<< HEAD
-        //markers: true,
-=======
+        end: 'top top',
+        animation: bodyAni,
         markers: true,
->>>>>>> parent of 1b4db02 (abc)
+        //scrub: true,
+        //enter leave enterback leaveback
+        toggleActions: 'play none none reverse',
+
+        onEnter: () => {
+            mode = 'white';
+
+            changeTheme(mode);
+        },
+        onEnterBack: () => {
+            mode = 'dark';
+
+            changeTheme(mode);
+        },
+    });
+}
+
+function introAnimation() {
+    const intro = gsap
+        .timeline()
+        .set('.intro_text p span', {
+            yPercent: 100,
+        })
+        .set(
+            '.intro_img figure',
+            {
+                opacity: 0,
+                xPercent: 10,
+            },
+            0
+        )
+        .to('.intro_text p span', {
+            yPercent: 0,
+            stagger: {
+                //amount: 1,
+                each: 0.1,
+            },
+        })
+        .to('.intro_img figure', {
+            opacity: 1,
+            xPercent: 0,
+        });
+
+    ScrollTrigger.create({
+        trigger: '.intro',
+        start: 'top center',
+        end: '30% center',
+        animation: intro,
+        //markers: true,
     });
 }
 
 headerFixed();
+bodyBgAnimation();
 mainBgAnimation();
 scrollAnimation();
+introAnimation();
 markers();
